d56a0c976860248a2d3e5b9829944b48aa305da6
Zachary Light <zacharylight15@mittymonarch.com>
2018-11-03 18:33:49 -0700

    updating manifest and build to avoid merge conflict later

---
+++ app/src/main/java/com/p2t/p2t/PictureHandler.java
@@ -0,0 +1,16 @@
+package com.p2t.p2t;
+
+
+public class PictureHandler {
+    public PictureHandler(String base64Image)
+    {
+
+    }
+
+    private String getPostData(String base64Image)
+    {
+
+
+        return null;
+    }
+}
