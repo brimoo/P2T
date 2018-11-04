d00b21c5a3d15dbc6081aa7d2a2dd4e8038f9cb2
Zachary Light <zacharylight15@mittymonarch.com>
2018-11-03 18:43:04 -0700

    fixing build.gradle

---
+++ app/src/main/java/com/p2t/p2t/GetOAuthToken.java
@@ -0,0 +1,23 @@
+package com.p2t.p2t;
+
+import android.os.AsyncTask;
+
+import com.google.android.gms.auth.GoogleAuthException;
+
+import java.io.IOException;
+
+public class GetOAuthToken extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected Void doInBackground(Void... params)
+    {
+        try{
+
+        }
+        catch(IOException e)
+        {
+
+        }
+        return null;
+    }
+}
