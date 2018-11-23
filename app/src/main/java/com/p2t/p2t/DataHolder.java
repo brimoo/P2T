eca56dee33b0c090d30e10c36c46eb13dc04b92e
Jadon Hansell <alcamore@gmail.com>
2018-11-22 17:51:13 -0800

    Removed DataHolder

--- app/src/main/java/com/p2t/p2t/DataHolder.java
+++
@@ -1,7 +0,0 @@
-package com.p2t.p2t;
-
-public class DataHolder {
-    private static String data = "";
-    public static void setData(String data) { DataHolder.data = data; }
-    public static String getData() { return data; }
-}
